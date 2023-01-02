28a37c89497ad0db18f49e299d4be91df1c740ab
DESKTOP-7B57JU3\wjddn <aronlee5263@gmail.com>
2023-01-03 04:35:55 +0900

    셀레니움으로 이미지 검색하고, 이미지를 클릭해서 우측에 크게 나오는 이미지의 src를 가져오는것 까지 성공
    다만
    터미널에서 python.google.py로 실행하는것과 (해당 파일의 디렉토리에 있어야함)
    우측 상단에 실행버튼을 눌러서 실행하는것이 차이가 있음. 이 차이는 구글 드라이버 문제같은데 해결을 해야할듯.
    현재는 우측 상단의 실행버튼으로 진행했다.

--- imageCrawling/selenium/google.py
+++ imageCrawling/selenium/google.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+import time
 
 # driver = webdriver.Chrome()
 driver = webdriver.Chrome(r"C:\Users\wjddn\Documents\AwesomeProject-0534\imageCrawling\selenium\chromedriver.exe")
@@ -9,8 +10,12 @@ driver.get("https://www.google.co.kr/imghp?hl=ko&authuser=0&ogbl")
 elem = driver.find_element(By.NAME, "q") #위에서 찾은 구글 주소에 개발자 도구란에 요소 q로 찾음.#
 elem.send_keys("테니스")  # 키보드 입력값 전송#
 elem.send_keys(Keys.RETURN)
-driver.find_elements_by_css_selector(".rg_i.Q4LuWd")[1].click()
-driver.find_elements_by_css_selector(".n3VNCb.KAlRDb")
+driver.find_elements_by_css_selector(".rg_i.Q4LuWd")[0].click()
+time.sleep(3)
+# print(driver.find_element_by_css_selector(".n3VNCb").get_attribute("src"))
+
+print(driver.find_element(By.CSS_SELECTOR, ".n3VNCb.KAlRDb").get_attribute("src"))
+
 
 # assert "Python" in driver.title
 # elem = driver.find_element(By.NAME, "q")
