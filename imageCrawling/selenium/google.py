fa1e7584ec31c6bf3339b4f11fc8cab2e7890bcf
DESKTOP-7B57JU3\wjddn <aronlee5263@gmail.com>
2023-01-03 20:48:45 +0900

    하나의 이미지 다운에서 여러개의 이미지 다운하도록 변경
    중간에 클래스를 하나만 줄여야 동작이 되었음. 왜냐하면 이미지들마다
    어떤것은 .n3VNCb.KAlRDb 처럼 두개의 클래스가 있는것이 있고
    어떤것은 .n3VNCb 하나만 있는 경우가 있기 떄문이다.

--- imageCrawling/selenium/google.py
+++ imageCrawling/selenium/google.py
@@ -12,16 +12,25 @@ driver.get("https://www.google.co.kr/imghp?hl=ko&authuser=0&ogbl")
 elem = driver.find_element(By.NAME, "q") #위에서 찾은 구글 주소에 개발자 도구란에 요소 q로 찾음.#
 elem.send_keys("테니스")  # 키보드 입력값 전송#
 elem.send_keys(Keys.RETURN)
-driver.find_elements_by_css_selector(".rg_i.Q4LuWd")[0].click()
-time.sleep(3)
-# print(driver.find_element_by_css_selector(".n3VNCb").get_attribute("src"))
-
-imgUrl = driver.find_element(By.CSS_SELECTOR, ".n3VNCb.KAlRDb").get_attribute("src")
+images = driver.find_elements_by_css_selector(".rg_i.Q4LuWd")
+count = 1
+for img in images:
+    img.click()
+    time.sleep(3)
+    # print(driver.find_element_by_css_selector(".n3VNCb").get_attribute("src"))
+    imgUrl = driver.find_element(By.CSS_SELECTOR, ".n3VNCb").get_attribute("src")
+    opener=urllib.request.build_opener()
+    opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
+    urllib.request.install_opener(opener)
+    urllib.request.urlretrieve(imgUrl, str(count) + ".jpg")
+    count = count + 1
  
-opener=urllib.request.build_opener()
-opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
-urllib.request.install_opener(opener)
-urllib.request.urlretrieve(imgUrl, "test.jpg")
+
+
+# opener=urllib.request.build_opener()
+# opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
+# urllib.request.install_opener(opener)
+# urllib.request.urlretrieve(imgUrl, "womentennis.jpg")
 
 # assert "Python" in driver.title
 # elem = driver.find_element(By.NAME, "q")
