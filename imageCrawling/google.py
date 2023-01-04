c0aad1e72f7a80035d31125ae569b2fa97c3bcba
Aron <aronlee5263@gmail.com>
2023-01-04 21:37:02 +0900

    1. gitignore 추가   2. 이미지 업로드  teachablemachine 링크
    머신으로 학습 시키고 그걸 클라우드로 내보내기 까지 한 상태임.
    https://teachablemachine.withgoogle.com/models/SGplRKomD/

--- imageCrawling/google.py
+++ imageCrawling/google.py
@@ -11,7 +11,7 @@ driver = webdriver.Chrome(r"C:\Users\이정운\Documents\Project\AwsomeProject-0
 
 driver.get("https://www.google.co.kr/imghp?hl=ko&authuser=0&ogbl")
 elem = driver.find_element(By.NAME, "q") #위에서 찾은 구글 주소에 개발자 도구란에 요소 q로 찾음.#
-elem.send_keys("테니스")  # 키보드 입력값 전송#
+elem.send_keys("악뮤 이찬혁")  # 키보드 입력값 전송#
 elem.send_keys(Keys.RETURN)
 
 
